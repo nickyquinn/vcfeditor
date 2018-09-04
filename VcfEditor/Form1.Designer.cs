a32e16c8f46d787feebb1bf13bd9d932e43f83ee
Nick Quinn <n.quinn@optoma.co.uk>
2018-09-04 07:54:21 +0100

    Initial

--- VcfEditor/Form1.Designer.cs
+++ VcfEditor/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtVcfLoc = new System.Windows.Forms.TextBox();
+            this.txtImgLoc = new System.Windows.Forms.TextBox();
+            this.btnVcfLoc = new System.Windows.Forms.Button();
+            this.btnImgLoc = new System.Windows.Forms.Button();
+            this.btnJustDoIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtVcfLoc
+            // 
+            this.txtVcfLoc.Location = new System.Drawing.Point(77, 12);
+            this.txtVcfLoc.Name = "txtVcfLoc";
+            this.txtVcfLoc.Size = new System.Drawing.Size(376, 20);
+            this.txtVcfLoc.TabIndex = 0;
+            // 
+            // txtImgLoc
+            // 
+            this.txtImgLoc.Location = new System.Drawing.Point(77, 38);
+            this.txtImgLoc.Name = "txtImgLoc";
+            this.txtImgLoc.Size = new System.Drawing.Size(376, 20);
+            this.txtImgLoc.TabIndex = 1;
+            // 
+            // btnVcfLoc
+            // 
+            this.btnVcfLoc.Location = new System.Drawing.Point(459, 12);
+            this.btnVcfLoc.Name = "btnVcfLoc";
+            this.btnVcfLoc.Size = new System.Drawing.Size(94, 23);
+            this.btnVcfLoc.TabIndex = 2;
+            this.btnVcfLoc.Text = "Select VCF";
+            this.btnVcfLoc.UseVisualStyleBackColor = true;
+            this.btnVcfLoc.Click += new System.EventHandler(this.btnVcfLoc_Click);
+            // 
+            // btnImgLoc
+            // 
+            this.btnImgLoc.Location = new System.Drawing.Point(459, 36);
+            this.btnImgLoc.Name = "btnImgLoc";
+            this.btnImgLoc.Size = new System.Drawing.Size(94, 23);
+            this.btnImgLoc.TabIndex = 3;
+            this.btnImgLoc.Text = "Select image";
+            this.btnImgLoc.UseVisualStyleBackColor = true;
+            this.btnImgLoc.Click += new System.EventHandler(this.btnImgLoc_Click);
+            // 
+            // btnJustDoIt
+            // 
+            this.btnJustDoIt.Location = new System.Drawing.Point(478, 74);
+            this.btnJustDoIt.Name = "btnJustDoIt";
+            this.btnJustDoIt.Size = new System.Drawing.Size(75, 23);
+            this.btnJustDoIt.TabIndex = 4;
+            this.btnJustDoIt.Text = "DO IT";
+            this.btnJustDoIt.UseVisualStyleBackColor = true;
+            this.btnJustDoIt.Click += new System.EventHandler(this.btnJustDoIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(559, 113);
+            this.Controls.Add(this.btnJustDoIt);
+            this.Controls.Add(this.btnImgLoc);
+            this.Controls.Add(this.btnVcfLoc);
+            this.Controls.Add(this.txtImgLoc);
+            this.Controls.Add(this.txtVcfLoc);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtVcfLoc;
+        private System.Windows.Forms.TextBox txtImgLoc;
+        private System.Windows.Forms.Button btnVcfLoc;
+        private System.Windows.Forms.Button btnImgLoc;
+        private System.Windows.Forms.Button btnJustDoIt;
     }
 }
 
--- VcfEditor/Form1.cs
+++ VcfEditor/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MixERP.Net.VCards;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,47 @@ namespace VcfEditor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnImgLoc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "(*.jpg)|*.jpg|All files (*.*)|*.*";
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtImgLoc.Text = fd.FileName;
+            }
+        }
+
+        private void btnVcfLoc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "(*.vcf)|*.vcf|All files (*.*)|*.*";
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtVcfLoc.Text = fd.FileName;
+            }
+        }
+
+        private void btnJustDoIt_Click(object sender, EventArgs e)
+        {
+            string contents = System.IO.File.ReadAllText(txtVcfLoc.Text, Encoding.UTF8);
+            VCard card = Deserializer.GetVCard(contents);
+
+            Byte[] imageBytes = System.IO.File.ReadAllBytes(txtImgLoc.Text);
+            string imageBase64 = Convert.ToBase64String(imageBytes);
+
+            var photo = new MixERP.Net.VCards.Models.Photo(true, System.IO.Path.GetExtension(txtImgLoc.Text), imageBase64);
+
+            card.Photo = photo;
+
+            string newContents = MixERP.Net.VCards.Serializer.VCardSerializer.Serialize(card);
+            string newPath = System.IO.Path.GetDirectoryName(txtVcfLoc.Text) + "\\" + 
+                System.IO.Path.GetFileNameWithoutExtension(txtVcfLoc.Text) + "-photoised" + 
+                System.IO.Path.GetExtension(txtVcfLoc.Text);
+
+            System.IO.File.WriteAllText(newPath, newContents);
+            MessageBox.Show("File saved as " + newPath);
         }
     }
 }
